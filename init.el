7625a736765c916ace875c5e9599d35e94ffcaaa
1amL0st <51456853+1amL0st@users.noreply.github.com>
2020-07-07 23:41:32 +0300

    Add files via upload

--- init.el
+++ init.el
@@ -16,46 +16,25 @@
 
 ;(package-refresh-contents)
 
-;;
-;; Helm
-;;
+;;;;;;;;;;;;
 
 (unless (package-installed-p 'helm)
 	(package-install 'helm))
 
-(unless (package-installed-p 'helm-projectile)
-	(package-install 'helm-projectile))
-(require 'helm-projectile)
-
 (require 'helm-config)
 (helm-mode 1)
 
-;;
-;; For working with 'workspaces'
-;;
+;;;;;;;;;;;;
 
 (unless (package-installed-p 'projectile)
   (package-install 'projectile))
 
-(defun lost-projectile-init ()
-  (projectile-mode +1)
-  ;(define-key projectile-mode-map (kbd "C-,") 'projectile-command-map)
-  (setq projectile-project-search-path '("~/.emacs.d/projects/" "~/.emacs.d/work/")))
+(projectile-mode +1)
+(setq projectile-project-search-path '("~/.emacs.d/projects/" "~/.emacs.d/work/"))
 
-(lost-projectile-init)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm-projectile helm bm projectile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(unless (package-installed-p 'helm-projectile)
+	(package-install 'helm-projectile))
+(require 'helm-projectile)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Common settings section
@@ -126,30 +105,39 @@
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Color scheme section
+;; Colors, visual appearence
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Leave all of them for now, maybe i'll use them ()_ but i am not sure.
+;(defvar user-theme-name "lost-ray-theme"
+;  "Path to user's color theme")
 
-(defvar user-theme-name "lost-ray-theme"
-  "Path to user's color theme")
+;(add-to-list 'load-path "~/.emacs.d/themes")
 
-(add-to-list 'load-path "~/.emacs.d/themes")
+;(unless (package-installed-p 'doom-themes)
+;	(package-install 'doom-themes))
+
+;(require 'doom-themes)
 
 ;Initialize text color settings
 (defun init-text-color-settings()
   "Sets my appearence for the text"
-  (setq-default tab-width 2)
-)
+  (setq-default tab-width 2))
+
+(unless (package-installed-p 'gruvbox-theme)
+	(package-install 'gruvbox-theme))
+
+(require 'gruvbox-theme)
 
 (defun init-select-font ()
 	(if (string= system-type "gnu/linux")
-			(set-default-font "Liberation Mono 11" nil t))
+			(set-default-font "Source Code Pro 11" nil t))
 	(if (string= system-type "windows-nt")
 		  (set-default-font "Lucida Console 13" nil t)))
 
 (defun lost-init-hook ()
-  (require 'lost-ray-theme)
-  (init-select-font)
   (init-text-color-settings)
-)
+  (init-select-font))
 
 (add-hook 'after-init-hook 'lost-init-hook)
+
+(custom-set-variables)
